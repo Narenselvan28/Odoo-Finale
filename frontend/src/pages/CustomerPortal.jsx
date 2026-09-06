a57d85d7270ae9dce545a4c8dccd6d1d9b3a58a4
Naren Selvan <narenselvan77@gmail.com>
2026-09-06 09:56:08 +0530

    fix: remove breadcrumb bar from end-user Customer Portal

--- frontend/src/pages/CustomerPortal.jsx
+++ frontend/src/pages/CustomerPortal.jsx
@@ -259,11 +259,6 @@ const CustomerPortal = () => {
         </div>
       </header>
 
-      {/* ===== BREADCRUMB ===== */}
-      <div className="breadcrumb" style={{ marginTop: "16px" }}>
-        <Link to="/dashboard">Home</Link> <span className="sep">/</span> <span>Customer Negotiation Portal</span> <span className="sep">/</span> <span className="current">Quotation #{quoteData.quotation_number || quoteData.id}</span>
-      </div>
-
       {/* Re-Approval Notice Alert if triggered */}
       {reApprovalNotice && (
         <div
